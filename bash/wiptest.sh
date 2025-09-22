5e5fb9d6cdb65032c1e7f9eb00a82e238add0404
Shane Lenagh <shanelenagh@gmail.com>
2025-09-22 14:21:12 -0500

    Formatting of test for better readability (makes schema clearer)

--- bash/wiptest.sh
+++ bash/wiptest.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
-# SCHEMA: [ { command: str, <commandParameters>=[{<mcpParameter: str>, <commandSwitch: str>}, ...]>, mcpToolSpec: {...} } ]
-#set -x
 #echo '{"jsonrpc": "2.0", "id": 1, "method": "tools/list" }' \
-echo '{"jsonrpc": "2.0", "id": 1, "method": "tools/call", "params": { "name": "bigThing", "arguments": { "dateParams": "-u" } } }' \
-    | ./mcpw.sh -d -t '[ { "command": "date", '\
-'"commandParameters": [{ "mcpParameter": "dateParams" }], '\
-'"mcpToolSpec": { "name": "bigThing", "description": "Awesome method", '\
-'  "inputSchema": { '\
-'    "properties": { "dateParams": { "title": "Date parameters", "type": "string" } }, '\
-'    "required": [], "type": "object" '\
-'  } '\
-'}'\
+echo '{"jsonrpc": "2.0", "id": 1, "method": "tools/call", "params": { "name": "bigThing", "arguments": { "dateParams": "-u" } } }'\
+    | ./mcpw.sh -d -t '[ { "command": "date",'\
+'   "commandParameters": [{ "mcpParameter": "dateParams" }],'\
+'   "mcpToolSpec": { "name": "bigThing", "description": "Awesome method",'\
+'       "inputSchema": { '\
+'           "properties": { "dateParams": { "title": "Date parameters", "type": "string" } },'\
+'           "required": [], "type": "object" '\
+'       }'\
+'   }'\
 '}]'
