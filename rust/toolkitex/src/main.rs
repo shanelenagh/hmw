c5be3982a43319625d0b768b63d7c620bd6a19dc
Shane Lenagh <shanelenagh@gmail.com>
2025-10-14 11:17:14 -0500

    Adding tracing/logging to default, to see streamble_http details in action

--- rust/toolkitex/src/main.rs
+++ rust/toolkitex/src/main.rs
@@ -2,12 +2,21 @@ use rmcp::transport::streamable_http_server::{
     StreamableHttpService, session::local::LocalSessionManager
 };
 mod greeter;
+use tracing_subscriber::{prelude::__tracing_subscriber_SubscriberExt, util::SubscriberInitExt};
 
 const BIND_ADDRESS: &str = "127.0.0.1:8000";
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-     //println!("Hello, world!");
+    //println!("Hello, world!");
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| "your_app_name=debug,tower_http=debug,axum=debug".into()),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
     let service = StreamableHttpService::new(
         || Ok(greeter::Greeter::new()),
         LocalSessionManager::default().into(),
