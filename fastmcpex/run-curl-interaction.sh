8256881ec5291e6a4fe14fdd0335391d644a1be4
Shane Lenagh <shanelenagh@gmail.com>
2025-10-15 13:46:57 -0500

    Tweaked existing curl example scripts, and added one master runner script to show a complete interaction (with automatic session ID capturing from the init request)

---
+++ fastmcpex/run-curl-interaction.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+init_output=$(. ./curl-init.sh 2>&1)
+#echo ">>>> Init response: $init_output"
+session_id_hdr=$(echo $init_output | grep -oP 'mcp-session-id: [\d\w]+')
+echo ">>>> Initialized, and got Session ID Header: $session_id_hdr"
+echo ">>>> Sending ACK of initialization"
+. ./curl-nowinitialized.sh -H "$session_id_hdr"
+echo ">>>> Listing tools"
+. ./curl-listtools.sh -H "$session_id_hdr"
+echo ">>>> Calling tool"
+. ./curl-toolcall.sh -H "$session_id_hdr"
