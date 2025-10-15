27921efa29b3139e57be8fd67cba40049829f773
Shane Lenagh <shanelenagh@gmail.com>
2025-10-15 15:53:06 -0500

    Formatting big curl payloads for readability

--- fastmcpex/curl-init.sh
+++ fastmcpex/curl-init.sh
@@ -2,4 +2,6 @@
 curl -v http://localhost:8000/mcp "$@" \
     -H "Accept: text/event-stream, application/json" \
     -H "Content-Type: application/json" \
-    -d '{ "jsonrpc": "2.0", "method": "initialize", "id": 1, "params": { "protocolVersion": "2024-11-05", "capabilities": {}, "clientInfo": { "name": "myClient", "version": "0.1" }}}'
+    -d '{ "jsonrpc": "2.0", "method": "initialize", "id": 1, 
+        "params": { "protocolVersion": "2024-11-05", "capabilities": {}, 
+            "clientInfo": { "name": "myClient", "version": "0.1" }}}'
--- fastmcpex/curl-toolcall.sh
+++ fastmcpex/curl-toolcall.sh
@@ -2,4 +2,6 @@
 curl http://localhost:8000/mcp "$@" \
     -H "Accept: text/event-stream, application/json" \
     -H "Content-Type: application/json" \
-    -d '{"jsonrpc": "2.0", "id": 1, "method": "tools/call", "params": { "name": "greet", "arguments": { "name": "Joedaddy" } } }'
+    -d '{"jsonrpc": "2.0", "id": 1, "method": "tools/call", 
+        "params": { "name": "greet", 
+            "arguments": { "name": "Joedaddy" } } }'
