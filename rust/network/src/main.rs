8c182ab48c4886cd0bd0772ceca2dd8e10955451
Shane Lenagh <shanelenagh@gmail.com>
2025-10-16 12:09:10 -0500

    Baselining basic session map

--- rust/network/src/main.rs
+++ rust/network/src/main.rs
@@ -2,11 +2,33 @@ use axum::{
     routing::get,
     Router,
 };
+use std::{
+    collections::HashMap,
+    sync::Mutex,
+};
+use lazy_static::lazy_static;
+use uuid::Uuid;
+
+
+#[derive(Debug)]
+struct Session {
+}
+
+lazy_static! {
+    static ref SESSION_MAP: Mutex<HashMap<String, Session>> = {
+        Mutex::new(HashMap::new())
+    };
+}
 
 #[tokio::main]
 async fn main() {
     // build our application with a single route
     let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+
+    // Session map test
+    let mut session_guard = SESSION_MAP.lock().unwrap();
+    session_guard.insert(Uuid::new_v4().to_string(), Session {});
+    println!("Session map contents: {:?}", *session_guard);
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
