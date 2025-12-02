d7550282399df8ccbd3800652891e0d3f3d91928
Shane Lenagh <shanelenagh@gmail.com>
2025-12-02 14:05:11 -0600

    Added session support

--- rust/network/src/main.rs
+++ rust/network/src/main.rs
@@ -2,6 +2,8 @@ use argh::FromArgs;
 use axum::{
     extract,
     routing::post,
+    http::{HeaderMap, StatusCode},
+    response::{IntoResponse, Json},
     Router
 };
 use mcpw_common::*;
@@ -16,12 +18,12 @@ use tracing::{debug};
 
 
 // TODO: Fill out and use this for MCP sessions
-// #[derive(Debug, Clone)]
-// struct Session {
-// }
+#[derive(Debug, Clone)]
+struct Session {
+}
 
 /// TODO: Move to common lib !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-#[derive(FromArgs)]
+#[derive(FromArgs, Clone)]
 struct Args {
     #[argh(option, short='t', description="array of tool specification command wrapper mappings in JSON format: [ {{ \"command\": \"scriptOrExecutable\", <\"command_parameters\": [ <\"mcp_param\": \"nameOfMcpMethodArgParameterToMapToCommandParam\">, <\"command_param\": \"staticCommandSwitchOrSwitchForMcpParameter\" ]>, \"mcp_tool_spec\": {{ mcpToolSpecJsonPerOfficialMcpSchema... }} }} , ... ]")]
     tool_specs: String, 
@@ -30,14 +32,17 @@ struct Args {
     debug: bool,
     #[cfg(feature = "debug_log")]
     #[argh(switch, short='p', description="pretty print log (including console ASCII coloring)")]
-    pretty: bool    
+    pretty: bool,    
+    #[argh(switch, short='s', description="use sessions (via Mcp-Session-Id header)")]
+    use_session: bool  
 }
 
 #[derive(Clone)]
 struct AppState {
+    args: Args,
     tool_spec_map: HashMap<String, ToolDefinition>,
     tools: Vec<Tool>,
-    //sessions: HashMap<String, Session>
+    sessions: HashMap<String, Session>
 }
 
 
@@ -49,9 +54,10 @@ async fn main()  -> result::Result<(), Box<dyn std_error::Error>> {
         return Err(("Can't parse tool list (confirm schema with help CLI option): ".to_owned() + &args.tool_specs).into());
     };
     let state = AppState { 
+        args: args,
         tool_spec_map: tool_definitions.iter().map(|tool| (tool.mcp_tool_spec.name.clone(), tool.clone())).collect(),
         tools: tool_definitions.iter().map(|tool| tool.mcp_tool_spec.clone()).collect::<Vec<Tool>>(),
-        //sessions: HashMap::new()
+        sessions: HashMap::new()
     };    
     // build our application with a single route
     let app = Router::new()
@@ -65,30 +71,38 @@ async fn main()  -> result::Result<(), Box<dyn std_error::Error>> {
     return Ok(())
 }
 
-async fn mcp_route(extract::State(state): extract::State<AppState>, extract::Json(payload): extract::Json<JsonrpcRequest>) 
-    -> result::Result<extract::Json<JsonRpcServerResult>, extract::Json<JsonrpcError>> 
+async fn mcp_route(extract::State(mut state): extract::State<AppState>, extract::Json(payload): extract::Json<JsonrpcRequest>) 
+    -> axum::response::Response 
 {
     let string_payload = serde_json::to_string(&payload).unwrap();
     debug!("Received MCP data with method [{}] and full payload: {}", payload.method, &string_payload); 
     match payload.method.as_str() {
         "initialize" => {
-            return Ok(axum::Json(mcp_init(payload.id, env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION"))));
+            let mut headers = HeaderMap::new();
+            if state.args.use_session {
+                let session_id = uuid::Uuid::new_v4().to_string();
+                headers.insert("Mcp-Session-Id", session_id.parse().unwrap());
+                state.sessions.insert(session_id, Session{});
+            } 
+            return (StatusCode::OK, headers, Json(mcp_init(payload.id, 
+                env!("CARGO_PKG_NAME"), env!("CARGO_PKG_VERSION")))).into_response();
         },
         "tools/call" => {
             let Ok(tool_call_request) = serde_json::from_str::<CallToolRequest>(&string_payload) else {
-                return Err(axum::Json(jsonrpc_error(
-                    RequestId::from(-1), -32700, "Parsing of tool call request failed: ".to_string()+&string_payload)));
+                return (StatusCode::UNPROCESSABLE_ENTITY, Json(jsonrpc_error(RequestId::from(-1), -32700, 
+                    "Parsing of tool call request failed: ".to_string()+&string_payload))).into_response();
             };
             return match mcp_handle_tool_call(payload.id, &tool_call_request, &state.tool_spec_map) {
-                Ok(response) => Ok(axum::Json(response)),
-                Err(err) => Err(axum::Json(err))
+                Ok(response) => (StatusCode::OK, Json(response)).into_response(),
+                Err(err) => (StatusCode::INTERNAL_SERVER_ERROR, Json(err)).into_response()
             }   
         },  
         "tools/list" => {
-            return Ok(axum::Json(mcp_tools_list(payload.id, &state.tools)));
+            return (StatusCode::OK, Json(mcp_tools_list(payload.id, &state.tools))).into_response();
         },              
         _ => {
-            return Err(axum::Json(jsonrpc_error(payload.id, -32601, "Method not found: ".to_string() + payload.method.as_str())));
+            return (StatusCode::UNPROCESSABLE_ENTITY, Json(jsonrpc_error(payload.id, -32601, 
+                "Method not found: ".to_string() + payload.method.as_str()))).into_response();
         }
     }
 }
